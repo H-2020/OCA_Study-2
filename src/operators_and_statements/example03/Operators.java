96f175b36367046844c077d8ccd0419b0b93af9b
Mert <mertdemirok80@gmail.com>
2021-02-08 21:07:20 +0100

    add example for arithmetic operators

---
+++ src/operators_and_statements/example03/Operators.java
@@ -0,0 +1,15 @@
+package operators_and_statements.example03;
+
+public class Operators {
+    public static void main(String[] args) {
+        short x = 14;
+        float y = 13;
+        double z = 30;
+
+        double sum = z + y;
+        System.out.println(sum);
+
+        double result = x*y/z;
+        System.out.println(result);
+    }
+}
