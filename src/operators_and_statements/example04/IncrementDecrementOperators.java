89cffebb9472c847071a55d1447a30433ae8faa3
Mert <mertdemirok80@gmail.com>
2021-02-10 05:33:24 +0100

    add multiple terms to the statement

---
+++ src/operators_and_statements/example04/IncrementDecrementOperators.java
@@ -0,0 +1,10 @@
+package operators_and_statements.example04;
+
+public class IncrementDecrementOperators {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = ++x * 5 / x-- + --x;
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+    }
+}
---
+++ src/operators_and_statements/example05/MultipleTerms.java
@@ -0,0 +1,11 @@
+package operators_and_statements.example05;
+
+public class MultipleTerms {
+    public static void main(String[] args) {
+        int x = 0;
+        for (long y = 0, z = 4; x < 5 && y < 10 ; x++, y++) {
+            System.out.print(y + " ");
+        }
+        System.out.print(x);
+    }
+}
