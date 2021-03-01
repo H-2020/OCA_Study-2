9333e3ca6a392182cb6452c56db424749e2717e2
Mert <mertdemirok80@gmail.com>
2021-03-01 12:19:45 +0100

    add instance block

--- src/methods_and_encapsulation/example04/RopeSwing.java
+++ src/methods_and_encapsulation/example04/RopeSwing.java
@@ -4,11 +4,20 @@ public class RopeSwing {
     private static Rope rope1 = new Rope();
     private static Rope rope2 = new Rope();
 
+    static {
+        System.out.println("static");
+    }
+
     {
+        System.out.println("instance block");
+    }
+
+    static {
         System.out.println(rope1.length);
     }
 
     public static void main(String[] args) {
+        RopeSwing ropeSwing = new RopeSwing();
         rope1.length = 2;
         rope2.length = 8;
         System.out.println(rope1.length);
