f7b65ca43a0d295d27afd97eb21e0b569e5d7df8
Mert <mertdemirok80@gmail.com>
2021-03-05 16:23:58 +0100

    add example for class cast exception

---
+++ src/class_design/example08/StringApp.java
@@ -0,0 +1,11 @@
+package class_design.example08;
+
+public class StringApp {
+    public static void main(String[] args) {
+        String str = (String)obj();
+    }
+
+    static Object obj(){
+        return new Object();
+    }
+}
