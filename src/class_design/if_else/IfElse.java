11b2dfb39e3d34735691a71e35ea6881386d2aa1
Mert <mertdemirok80@gmail.com>
2021-03-07 10:22:08 +0100

    add example for if and else statements

---
+++ src/class_design/if_else/IfElse.java
@@ -0,0 +1,11 @@
+package class_design.if_else;
+
+public class IfElse {
+    public static void main(String[] args) {
+        if(true);
+        else;
+
+        if(true)
+            if(false);
+    }
+}
