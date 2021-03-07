1ffc1b7b5ad8b062512c14ab95e40aceb2db6f52
Mert <mertdemirok80@gmail.com>
2021-03-07 19:24:12 +0100

    add example casting

---
+++ src/operators_and_statements/example11/Casting.java
@@ -0,0 +1,11 @@
+package operators_and_statements.example11;
+
+public class Casting {
+    short s1 = 200;
+    Integer s2 = 400;
+    Long s3 = (long) s1 + s2;
+//    String s4 = (String)(s3*s2); COMPILE ERROR
+
+
+
+}
