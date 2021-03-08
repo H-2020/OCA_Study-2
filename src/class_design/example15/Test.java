0a95735cd650643f6211fd8bb9b96bf53f9f8097
Mert <mertdemirok80@gmail.com>
2021-03-08 15:43:24 +0100

    exam 01 question 38

---
+++ src/class_design/example15/Test.java
@@ -0,0 +1,9 @@
+package class_design.example15;
+
+public class Test {
+    private int doStuff(){
+   //     private int x = 10; private not allowed here
+   //     return x++;
+        return -1;
+    }
+}
