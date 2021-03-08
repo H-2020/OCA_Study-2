6998fd980efa5d2e4dd4f5aa46bb2f49e790bd58
Mert <mertdemirok80@gmail.com>
2021-03-08 16:16:37 +0100

    exam 01 question 43

---
+++ src/switchs/Test1.java
@@ -0,0 +1,16 @@
+package switchs;
+
+public class Test1 {
+    public static void main(String[] args) {
+        char x = 1; // required char, byte, short, int, Character, Byte, Short, Integer, String, enum
+
+        switch (x){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+        }
+    }
+}
