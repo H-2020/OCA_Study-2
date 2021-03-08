bc8afb65553ce3d199741a6520fddc2107a24101
Mert <mertdemirok80@gmail.com>
2021-03-08 16:20:02 +0100

    exam 01 question 43

--- src/switchs/Test1.java
+++ src/switchs/Test1.java
@@ -2,8 +2,8 @@ package switchs;
 
 public class Test1 {
     public static void main(String[] args) {
-        char x = 1; // required char, byte, short, int, Character, Byte, Short, Integer, String, enum
-
+        //char x = 1; // required char, byte, short, int, Character, Byte, Short, Integer, String, enum
+        Integer x = new Integer("1");
         switch (x){
             case 1:
                 System.out.println("One");
