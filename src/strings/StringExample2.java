e565925e2a1f062ff5e580fc1a2ad056679444ab
Mert <mertdemirok80@gmail.com>
2021-03-08 12:15:25 +0100

    exam 01 question 30

---
+++ src/strings/StringExample2.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class StringExample2 {
+    public static void main(String[] args) {
+        String str = " ";
+        str.trim();
+        System.out.println(str.equals("") + " " + str.isEmpty());
+    }
+}
