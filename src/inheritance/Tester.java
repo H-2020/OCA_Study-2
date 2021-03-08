5bb800fcbb9aecd5f65ea744d83e59427977b494
Mert <mertdemirok80@gmail.com>
2021-03-08 22:34:07 +0100

    exam 01 question 61

---
+++ src/inheritance/Tester.java
@@ -0,0 +1,30 @@
+package inheritance;
+
+class SuperClass {
+    public static void display(){
+        System.out.println("SuperClass.display()");
+    }
+
+    //Method overloading of static method
+    public static void display(int a){
+        System.out.println("SuperClass.display(int): " + a);
+    }
+}
+
+class SubClass extends SuperClass {
+    //Not method overriding but hiding
+    public static void display(){
+        System.out.println("SubClass.display()");
+    }
+}
+
+public class Tester {
+    public static void main(String[] args) {
+        SuperClass object = new SubClass();
+
+        //SuperClass display method is called
+        //although object is of SubClass.
+        object.display();
+        object.display(1);
+    }
+}
