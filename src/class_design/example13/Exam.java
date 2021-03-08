b7e92b9741862dceba1140957388ca9750bd94cb
Mert <mertdemirok80@gmail.com>
2021-03-08 10:23:59 +0100

    exam 01 question 21

--- src/class_design/example13/Exam.java
+++ src/class_design/example13/Exam.java
@@ -1,7 +1,15 @@
 package class_design.example13;
 
 public class Exam {
+    int number = 3;
     public static void print(){
         System.out.println("exam");
+    //    print2();
+    //    number = 4;
+        new Exam().number = 5;
+    }
+
+    public void print2(){
+        System.out.println("exam2");
     }
 }
--- src/class_design/example13/Test.java
+++ src/class_design/example13/Test.java
@@ -7,11 +7,24 @@ public class Test {
 
         System.out.println(isAvailable + " ");
         isAvailable = test.doStuff();
+
         System.out.println(isAvailable);
         exam.print();
+
+        test.doAnotherStuff();
     }
 
     static boolean isAvailable = false;
+
+    public void doAnotherStuff(){
+        Exam exam = new Exam();
+        exam.print();
+
+        Test test = new Test();
+        test.doStuff();
+
+
+    }
 
     public static boolean doStuff(){
         return !isAvailable;
