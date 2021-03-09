e73e972c4cee6089eb5901a80f9fed5790c6756b
Mert <mertdemirok80@gmail.com>
2021-03-09 16:31:22 +0100

    exam 01 question 91

---
+++ src/arrays/Test5.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class Test5 {
+    public static void main(String[] args) {
+        String[] strs = {"A", "B"};
+        int idx = 0;
+        for(String s : strs){
+            strs[idx].concat(" element " + idx);
+            idx++;
+        }
+        System.out.println(idx);
+        for (idx = 0; idx < strs.length ; idx++) {
+            System.out.println(strs[idx]);
+        }
+    }
+}
