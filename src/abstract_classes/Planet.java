fb746563bb3ea7cb09c4c95c3085ca3690d6a0a2
Mert <mertdemirok80@gmail.com>
2021-03-09 19:23:29 +0100

    exam 01 question 94

---
+++ src/abstract_classes/Planet.java
@@ -0,0 +1,11 @@
+package abstract_classes;
+
+abstract class Planet {
+    protected void revolve(){}
+    abstract void rotate();
+}
+
+class Earth extends Planet{
+    protected void revolve(){}
+    protected void rotate(){};
+}
