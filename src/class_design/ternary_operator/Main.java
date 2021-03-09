f1042abfb38b190e4555ff02552068a58f5ed004
Mert <mertdemirok80@gmail.com>
2021-03-09 10:53:25 +0100

    add comment

--- src/class_design/ternary_operator/Main.java
+++ src/class_design/ternary_operator/Main.java
@@ -3,9 +3,10 @@ package class_design.ternary_operator;
 public class Main {
     public static void main(String[] args) {
         int x = 100;
-        int a = x++;
-        int b = ++x;
-        int c = x++;
+        int a = x++; // 100
+        int b = ++x; // 102
+        int c = x++; // 102
+        // x = 103
         //System.out.println(b);
         int d = (a < b) ? (a < c) ? a: (b < c) ? b : c : x;
         System.out.println(d);
