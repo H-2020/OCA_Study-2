ac8e074f0e768a9784af4cbc1fee0de5a8197a95
Mert <mertdemirok80@gmail.com>
2021-03-09 11:36:50 +0100

    add example for inheritance

---
+++ src/inheritance/sub/SubClass.java
@@ -0,0 +1,35 @@
+package inheritance.sub;
+
+import inheritance.SuperClass;
+
+public class SubClass extends SuperClass {
+    //Not method overriding but hiding
+    public void display(){
+        System.out.println("SubClass.display()");
+    }
+
+    public static void main(String[] args) {
+        SuperClass object = new SubClass();
+
+        //SuperClass display method is called
+        //although object is of SubClass.
+        object.display(); // metodlar object tipine gore cagirir
+        object.display(1);
+    }
+}
+
+class School {
+
+    public static void main(String[] args) {
+        SuperClass object = new SubClass();
+        //SuperClass.display(); // metodlar object tipine gore cagirir
+        object.display();
+    }
+
+    public void test(){
+        SuperClass object = new SubClass();
+        //SuperClass.display(); // metodlar object tipine gore cagirir
+        object.display();
+    }
+
+}
