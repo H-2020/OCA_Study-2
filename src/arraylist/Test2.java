11755d940955635ed7892dc929c55eb9c3dc0cfd
Mert <mertdemirok80@gmail.com>
2021-03-09 15:54:58 +0100

    exam 01 question 90

---
+++ src/arraylist/Test2.java
@@ -0,0 +1,19 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test2 {
+    public static void main(String[] args) {
+        String[] arr = {"Hi", "How", "Are", "You"};
+        List<String> arrList = new ArrayList<>(Arrays.asList(arr));
+
+        if(arrList.removeIf(s -> {
+            System.out.print(s);
+            return s.length() <= 2;
+        })){
+            System.out.println(" removed");
+        };
+    }
+}
