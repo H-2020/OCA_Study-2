ade7e7a2375707ea03a1afa0e605f08a48c84fa0
Mert <mertdemirok80@gmail.com>
2021-03-10 07:20:54 +0100

    example for static import

---
+++ src/arrays/Test6.java
@@ -0,0 +1,10 @@
+package arrays;
+
+import java.util.Arrays;
+import static java.util.Arrays.asList;
+
+public class Test6 {
+    public static void main(String[] args) {
+        Arrays.asList(1);
+    }
+}
