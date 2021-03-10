c7f4dc7210d0c97f63c3ac21b366ff337dee1ab4
Mert <mertdemirok80@gmail.com>
2021-03-10 07:24:58 +0100

    overloading and varargs

--- src/arrays/Test6.java
+++ src/arrays/Test6.java
@@ -6,5 +6,16 @@ import static java.util.Arrays.asList;
 public class Test6 {
     public static void main(String[] args) {
         Arrays.asList(1);
+        fly(new int[]{1, 2, 3});
+        fly(4, 5, 6, 7);
     }
+
+    public static void fly(int... lengths){
+        System.out.println(lengths.length);
+    }
+
+    // DOES NOT COMPILE
+//    public static void fly(int[] lengths){
+//        System.out.println(lengths.length);
+//    }
 }
