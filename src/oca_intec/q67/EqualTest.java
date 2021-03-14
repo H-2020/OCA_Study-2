08c0ac334727a06d315c815879f7fffe41e6c6fd
Mert <mertdemirok80@gmail.com>
2021-03-14 18:15:21 +0100

    equals method

---
+++ src/oca_intec/q67/EqualTest.java
@@ -0,0 +1,10 @@
+package oca_intec.q67;
+
+public class EqualTest {
+    public static void main(String[] args) {
+        Integer i = new Integer(1);
+        Long m = new Long(1);
+        if(i.equals(m)) System.out.println("equal");
+        else System.out.println("not equal");
+    }
+}
