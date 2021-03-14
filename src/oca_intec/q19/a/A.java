c0aee43cf42ca9e1eb78148429a04e6fcbb72ec1
Mert <mertdemirok80@gmail.com>
2021-03-14 08:18:54 +0100

    default constractors in different packages not allowed

---
+++ src/oca_intec/q19/a/A.java
@@ -0,0 +1,9 @@
+package oca_intec.q19.a;
+
+public class A {
+    public A() { }
+
+    public void print(){
+        System.out.println("A");
+    }
+}
---
+++ src/oca_intec/q19/b/B.java
@@ -0,0 +1,14 @@
+package oca_intec.q19.b;
+import oca_intec.q19.a.*;
+
+public class B extends A {
+   public B() { }
+
+    public void print(){
+        System.out.println("B");
+    }
+
+    public static void main(String[] args) {
+        new B();
+    }
+}
