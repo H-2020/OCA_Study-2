174ab984bb29952060148c34f04b74b84a9e0716
Mert <mertdemirok80@gmail.com>
2021-03-15 10:41:56 +0100

    unreachable variable

---
+++ src/arrays/Test7.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class Test7 {
+    public static void main(String[] args) {
+        int data[] = {2010, 2013, 2014, 2015, 2014};
+        int key = 2014;
+        int count = 0;
+        for (int e:data
+        ) {
+            if(e != key){
+                continue;
+            //     count++;  UNREACHABLE VARIABLE
+            }
+        }
+        System.out.println(count + "Found");
+    }
+}
