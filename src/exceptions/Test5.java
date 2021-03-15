06e31ff4147f95091c9d65886babd60981a8793c
Mert <mertdemirok80@gmail.com>
2021-03-15 19:39:13 +0100

    add example for exception

---
+++ src/exceptions/Test5.java
@@ -0,0 +1,22 @@
+package exceptions;
+
+public class Test5 {
+    public static void main(String[] args) {
+        try {
+            method1();
+        } catch (MyException e) {
+            System.out.println("A");
+        }
+    }
+
+    public static void method1(){
+        try {
+            throw Math.random() > 0.5 ? new MyException() : new RuntimeException();
+        } catch (RuntimeException re) {
+            System.out.println("B");
+        }
+    }
+}
+
+class MyException extends RuntimeException{}
+
