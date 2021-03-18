4c6768b0292e46a1e92699a177079eb7e0e8d9a8
Mert <mertdemirok80@gmail.com>
2021-03-18 20:54:49 +0100

    default class can not be accessed from another package

---
+++ src/mixed/q09/Main.java
@@ -0,0 +1,9 @@
+package mixed.q09;
+import mixed.q15.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        Test test = new Test("Student");
+        System.out.println(test);
+    }
+}
---
+++ src/mixed/q15/Test.java
@@ -0,0 +1,16 @@
+package mixed.q15;
+
+public class Test {
+    String name;
+
+    public Test(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
